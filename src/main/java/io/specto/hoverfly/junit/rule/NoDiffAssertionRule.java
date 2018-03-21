a335e96ff5a7f6c2dcadfb51b23b1234718d0fec
Matous Jobanek <mjobanek@redhat.com>
2018-03-21 15:44:54 +0100

    chore(diff): removes shouldClean constructor parameter from NoDiffAssertionRule
    Former-commit-id: fbe2b83867b69f7e65e17cfef59596ce314ac766

--- src/main/java/io/specto/hoverfly/junit/rule/NoDiffAssertionRule.java
+++ src/main/java/io/specto/hoverfly/junit/rule/NoDiffAssertionRule.java
@@ -14,58 +14,54 @@ import org.junit.runners.model.Statement;
 public class NoDiffAssertionRule implements TestRule {
 
     private Hoverfly hoverfly;
-    private final boolean shouldClean;
     private HoverflyRule hoverflyRule;
 
     /**
      * Creates a rule with the given instance of {@link Hoverfly} that asserts that there was no diff between any of the
      * expected responses set by simulations and the actual responses returned from the real service.
-     * The parameter {@code shouldClean} says if the rule should clean all available diffs stored in Hoverfly before the
-     * test execution and when the assertion is performed (after the test method execution).
+     * The rule also removes (before and after the test execution) all possible diffs that are stored in Hoverfly.
+     * This ensures that all test runs are executed in isolated and clean environment.
      *
-     * @param hoverfly An instance of {@link Hoverfly} to be used for retrieving the diffs
-     * @param shouldClean if all available diffs should be removed.
+     * @param hoverfly
+     *     An instance of {@link Hoverfly} to be used for retrieving the diffs
      */
-    public NoDiffAssertionRule(Hoverfly hoverfly, boolean shouldClean) {
+    public NoDiffAssertionRule(Hoverfly hoverfly) {
         if (hoverfly == null) {
             throw new IllegalArgumentException("Hoverfly cannot be null");
         }
         this.hoverfly = hoverfly;
-        this.shouldClean = shouldClean;
     }
 
     /**
      * Creates a rule with the given instance of {@link HoverflyRule} that asserts that there was no diff between any of the
      * expected responses set by simulations and the actual responses returned from the real service.
-     * The parameter {@code shouldClean} says if the rule should clean all available diffs stored in Hoverfly before the
-     * test execution and when the assertion is performed (after the test method execution).
+     * The rule also removes (before and after the test execution) all possible diffs that are stored in Hoverfly.
+     * This ensures that all test runs are executed in isolated and clean environment.
      *
-     * @param hoverflyRule An instance of {@link HoverflyRule} to be used for retrieving the diffs
-     * @param shouldClean if all available diffs should be removed.
+     * @param hoverflyRule
+     *     An instance of {@link HoverflyRule} to be used for retrieving the diffs
      */
-    public NoDiffAssertionRule(HoverflyRule hoverflyRule, boolean shouldClean) {
+    public NoDiffAssertionRule(HoverflyRule hoverflyRule) {
         if (hoverflyRule == null) {
             throw new IllegalArgumentException("HoverflyRule cannot be null");
         }
         this.hoverflyRule = hoverflyRule;
-        this.shouldClean = shouldClean;
     }
 
     /**
      * Creates a rule with the given instance of {@link HoverflyConfig} that asserts that there was no diff between any of the
      * expected responses set by simulations and the actual responses returned from the real service.
-     * The parameter {@code shouldClean} says if the rule should clean all available diffs stored in Hoverfly before the
-     * test execution and when the assertion is performed (after the test method execution).
+     * The rule also removes (before and after the test execution) all possible diffs that are stored in Hoverfly.
+     * This ensures that all test runs are executed in isolated and clean environment.
      *
-     * @param hoverflyConfig An instance of {@link HoverflyConfig} to be used for retrieving the diffs
-     * @param shouldClean if all available diffs should be removed.
+     * @param hoverflyConfig
+     *     An instance of {@link HoverflyConfig} to be used for retrieving the diffs
      */
-    public NoDiffAssertionRule(HoverflyConfig hoverflyConfig, boolean shouldClean) {
+    public NoDiffAssertionRule(HoverflyConfig hoverflyConfig) {
         if (hoverflyConfig == null) {
             throw new IllegalArgumentException("HoverflyConfig cannot be null");
         }
         hoverfly = new Hoverfly(hoverflyConfig, HoverflyMode.DIFF);
-        this.shouldClean = shouldClean;
     }
 
     @Override
@@ -73,16 +69,14 @@ public class NoDiffAssertionRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                if (shouldClean){
-                    cleanDiffs();
-                }
+                cleanDiffs();
                 base.evaluate();
                 performAssertion();
             }
         };
     }
 
-    private void cleanDiffs(){
+    private void cleanDiffs() {
         if (hoverfly != null) {
             hoverfly.resetDiffs();
         } else {
@@ -90,9 +84,9 @@ public class NoDiffAssertionRule implements TestRule {
         }
     }
 
-    private void performAssertion(){
+    private void performAssertion() {
         if (hoverfly != null) {
-            hoverfly.assertThatNoDiffIsReported(shouldClean);
+            hoverfly.assertThatNoDiffIsReported(true);
         } else {
             hoverflyRule.assertThatNoDiffIsReported();
         }
